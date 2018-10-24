56986e16c2a7fc4ce97ad6c293250f08c6543048
J.R. Ruggiero <jrruggiero@shift3tech.com>
2018-10-24 13:40:45 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+// import { Provider } from 'react-redux';
+// import { createStore, applyMiddleware } from 'redux';
+// import ReduxThunk from 'redux-thunk';
+// import reducers from './reducers';
+// import Router from './Router';
+// import config from './config';
+
+
+class App extends Component {
+    render() {
+        // const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+        return (
+            <View>
+                <Text>RandoHelper</Text>
+            </View>
+            // <Provider store={store}>
+            //     <Router />
+            // </Provider>
+        );
+    }
+}
+
+export default App;
