c27c01fe651fe558e6932f3125434816c1b63f3d
J.R. Ruggiero <jrruggiero@shift3tech.com>
2018-10-24 14:21:18 -0700

    wired up redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-// import { Provider } from 'react-redux';
-// import { createStore, applyMiddleware } from 'redux';
-// import ReduxThunk from 'redux-thunk';
-// import reducers from './reducers';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import reducers from './reducers';
 // import Router from './Router';
 // import config from './config';
 
 
 class App extends Component {
     render() {
-        // const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+        const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
         return (
-            <View>
-                <Text>RandoHelper</Text>
-            </View>
-            // <Provider store={store}>
-            //     <Router />
-            // </Provider>
+            <Provider store={store}>
+                <View>
+                    <Text>RandoHelper</Text>
+                </View>
+            </Provider>
         );
     }
 }
